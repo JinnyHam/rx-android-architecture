55f415e3cae5103f27c2e678c86f2e6fc47d983c
Pawel Polanski <pawel.polanski@futurice.com>
2015-07-19 22:47:50 +0200

    Added stetho implementation

--- app/src/main/java/com/tehmou/rxbookapp/network/NetworkApi.java
+++ app/src/main/java/com/tehmou/rxbookapp/network/NetworkApi.java
@@ -2,10 +2,14 @@ package com.tehmou.rxbookapp.network;
 
 import com.tehmou.rxbookapp.pojo.GitHubRepository;
 
+import android.support.annotation.NonNull;
+
 import java.util.List;
 import java.util.Map;
 
 import retrofit.RestAdapter;
+import retrofit.client.Client;
+import rx.android.internal.Preconditions;
 
 /**
  * Created by ttuo on 06/01/15.
@@ -13,8 +17,11 @@ import retrofit.RestAdapter;
 public class NetworkApi {
     private final GitHubService gitHubService;
 
-    public NetworkApi() {
+    public NetworkApi(@NonNull Client client) {
+        Preconditions.checkNotNull(client, "Client cannot be null.");
+
         RestAdapter restAdapter = new RestAdapter.Builder()
+                .setClient(client)
                 .setEndpoint("https://api.github.com")
                 .setLogLevel(RestAdapter.LogLevel.NONE)
                 .build();
